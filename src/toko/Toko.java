6b705bd6549b7ffd30ae2cf19e55d23da5272802
ayissubagyo007@gmail.com <ayissubagyo007@gmail.com>
2019-09-27 14:57:34 +0700

    netben

---
+++ src/toko/Toko.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toko;
+import toko.koneksi;
+import toko.menu;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ASUS
+ */
+public class Toko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new login().setVisible(true);
+        
+        // TODO code application logic here
+    }
+    
+}
